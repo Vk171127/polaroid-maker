62368f04aad5e054dcb7d49f34ac2c71c0f74302
vignesh <vigneshromen7@gmail.com>
2026-08-15 19:40:37 +0530

    feat: integrate image cropper

---
+++ src/components/CropImage.tsx
@@ -0,0 +1,99 @@
+import Button from "@/components/Button";
+import { useEffect, useState } from "react";
+import Cropper, { type Area, type Point } from "react-easy-crop";
+
+interface CropImageProps {
+  img: string;
+  onCropDone: (croppedImage: string) => void;
+}
+
+const CropImage = ({ img, onCropDone }: CropImageProps) => {
+  const [crop, setCrop] = useState<Point>({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area | null>(null);
+  const [croppedImage, setCroppedImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (croppedImage) {
+        URL.revokeObjectURL(croppedImage);
+      }
+    };
+  }, [croppedImage]);
+
+  function onCropComplete(_: Area, croppedPixels: Area) {
+    setCroppedAreaPixels(croppedPixels);
+  }
+
+  async function showCroppedImage() {
+    if (!croppedAreaPixels) {
+      return;
+    }
+    const result = await getCroppedImg(img, croppedAreaPixels);
+
+    if (result) {
+      onCropDone(result);
+    }
+  }
+
+  async function getCroppedImg(imageSrc: string, pixelCrop: Area) {
+    const image = await createImage(imageSrc);
+
+    const croppedCanvas = document.createElement("canvas");
+    const croppedCtx = croppedCanvas.getContext("2d");
+
+    if (!croppedCtx) {
+      return null;
+    }
+
+    croppedCanvas.width = pixelCrop.width;
+    croppedCanvas.height = pixelCrop.height;
+
+    croppedCtx.drawImage(
+      image,
+      pixelCrop.x,
+      pixelCrop.y,
+      pixelCrop.width,
+      pixelCrop.height,
+      0,
+      0,
+      pixelCrop.width,
+      pixelCrop.height,
+    );
+
+    return new Promise<string | null>((resolve) => {
+      croppedCanvas.toBlob((file) => {
+        resolve(file ? URL.createObjectURL(file) : null);
+      }, "image/jpeg");
+    });
+  }
+
+  function createImage(url: string) {
+    return new Promise<HTMLImageElement>((resolve, reject) => {
+      const image = new Image();
+      image.addEventListener("load", () => resolve(image));
+      image.addEventListener("error", reject);
+      image.setAttribute("crossOrigin", "anonymous");
+      image.src = url;
+    });
+  }
+
+  return (
+    <>
+      <div className="cropper h-[70vh] relative w-full">
+        <Cropper
+          image={img}
+          crop={crop}
+          zoom={zoom}
+          aspect={3 / 4}
+          onCropChange={setCrop}
+          onZoomChange={setZoom}
+          onCropComplete={onCropComplete}
+        />
+      </div>
+      <Button name="Done" onClick={showCroppedImage} />
+    </>
+  );
+};
+
+export default CropImage;
--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/Button";
+import CropImage from "@/components/CropImage";
 import Uploader from "@/components/Uploader";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 type Step = "Upload" | "Preview" | "Order";
 
@@ -13,6 +14,39 @@ const Stepper = ({ requiredPhotoCount, close }: StepperProps) => {
   const [step, setStep] = useState<Step>("Upload");
   const [error, setError] = useState<string | null>(null);
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const [currentImgIndex, setCurrentImgIndex] = useState<number>(0);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [isCropping, setIsCropping] = useState(false);
+  const [croppedImage, setCroppedImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    const file = selectedFiles[currentImgIndex];
+    if (!file) {
+      setImageUrl(null);
+      return;
+    }
+
+    const url = URL.createObjectURL(file);
+    setImageUrl(url);
+
+    return () => {
+      URL.revokeObjectURL(url);
+    };
+  }, [selectedFiles, currentImgIndex]);
+
+  const thumbUrls = useMemo(
+    () => selectedFiles.map((file) => URL.createObjectURL(file)),
+    [selectedFiles],
+  );
+
+  useEffect(() => {
+    return () => thumbUrls.forEach((url) => URL.revokeObjectURL(url));
+  }, [thumbUrls]);
+
+  const handleCropdone = (croppedImage: string) => {
+    setCroppedImage(croppedImage);
+    setIsCropping(false);
+  };
 
   const handleClick = () => {
     if (selectedFiles.length !== requiredPhotoCount) {
@@ -34,7 +68,7 @@ const Stepper = ({ requiredPhotoCount, close }: StepperProps) => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col gap-4 h-full">
       <div>step 1,2,3 -- placeholder</div>
       {step === "Upload" && (
         <div>
@@ -43,6 +77,48 @@ const Stepper = ({ requiredPhotoCount, close }: StepperProps) => {
             requiredPhotoCount={requiredPhotoCount}
           />
           {error && <p className="text-red-500">{error}</p>}
+          <div className="flex gap-2 justify-center">
+            <Button onClick={close} variant="outline" name="Close" />
+            <Button onClick={handleClick} name="Proceed" />
+          </div>
+        </div>
+      )}
+      {step === "Preview" && (
+        <div className="flex flex-col gap-4 h-full">
+          {isCropping ? (
+            <CropImage img={imageUrl || ""} onCropDone={handleCropdone} />
+          ) : (
+            <>
+              <img
+                src={croppedImage || imageUrl || ""}
+                alt="Current preview"
+                className="w-20"
+              />
+
+              <Button
+                name="Crop / Adjust"
+                onClick={() => setIsCropping(true)}
+              />
+
+              <div className="flex gap-4 items-center w-full">
+                {thumbUrls.map((img, index) => {
+                  return (
+                    <button
+                      onClick={() => setCurrentImgIndex(index)}
+                      className="cursor-pointer"
+                      key={index}
+                    >
+                      <img
+                        src={img}
+                        alt={`Selected file ${index + 1}`}
+                        className="size-10"
+                      />
+                    </button>
+                  );
+                })}
+              </div>
+            </>
+          )}
           <div className="flex gap-2 justify-center">
             <Button onClick={close} variant="outline" name="Close" />
             <Button onClick={handleClick} name="Proceed" />
